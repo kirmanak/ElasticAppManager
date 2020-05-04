da37983169f90f071577714944c9c3a8d5a1431d
Kirill Kamakin <k.kamakin@protonmail.ch>
2020-05-04 18:22:56 +0300

    Initial commit
    Signed-off-by: Kirill Kamakin <k.kamakin@protonmail.ch>

---
+++ src/test/kotlin/ru/ifmo/kirmanak/manager/ManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.ifmo.kirmanak.manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
